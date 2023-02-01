86176b26facae8f628d8d5f343da5debaf01d2a5
verci-eth <hello@vercingetorige.tech>
2023-02-01 20:29:38 +0100

    project setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
-import '@/styles/globals.css'
+import './styles.css';
 
-export default function App({ Component, pageProps }) {
+function App({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
+export default App;
--- pages/_document.js
+++ pages/_document.js
@@ -2,8 +2,16 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html>
+      <Head>
+        <meta property="og:title" content="GPT-3 Writer" key="title"/>
+        <meta property="og:description" content="build with buildspace" key="description"/>
+        <meta
+          property="og:image"
+          content="https://cdn.buildspace.so/courses/gpt3-writer/project-og.jpg"
+        />
+        <meta name="twitter:card" content="summary_large_image"></meta>
+      </Head>
       <body>
         <Main />
         <NextScript />
