bc3259a58b979329fdf223f9012473d92e9d8bb0
verci-eth <hello@vercingetorige.tech>
2023-02-01 21:30:35 +0100

    style setup

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,50 @@
-import Head from 'next/head';
+import { useState } from 'react';
 import Image from 'next/image';
-import buildspaceLogo from '../assets/buildspace-logo.png';
+import verci from '../assets/verci.png';
 
 const Home = () => {
+  const [userInput, setUserInput] = useState('');
+
+  const onUserChangedText = (event) => {
+    setUserInput(event.target.value);
+  };
+
   return (
     <div className="root">
-      <Head>
-        <title>GPT-3 Writer | buildspace</title>
-      </Head>
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>sup, insert your headline here</h1>
+            <h1>suh dude</h1>
           </div>
           <div className="header-subtitle">
             <h2>insert your subtitle here</h2>
           </div>
         </div>
+        <div className="prompt-container">
+          <textarea 
+            placeholder="start typing here" 
+            className="prompt-box"
+            value={userInput}
+            onChange={onUserChangedText}
+          />
+          <div className="prompt-buttons">
+            <a className="generate-button" onClick={null}>
+              <div className="generate">
+                <p>generate</p>
+              </div>
+            </a>
+          </div>
+        </div>
       </div>
       <div className="badge-container grow">
         <a
-          href="https://buildspace.so/builds/ai-writer"
+          href="https://twitter.com/verci_eth"
           target="_blank"
           rel="noreferrer"
         >
           <div className="badge">
-            <Image src={buildspaceLogo} alt="buildspace logo" />
-            <p>build with buildspace</p>
+            <p>built by </p>
+            <Image src={verci} alt="buildspace logo" />
           </div>
         </a>
       </div>
